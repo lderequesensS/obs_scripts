284776701c520e9df6b7a30f2485cdaa80bfd578
lderequesens <leonardo.sobarzo@protonmail.com>
2023-11-10 08:55:04 -0300

    Fix bad date when searching prevSession

--- getPrevSessionDate.js
+++ getPrevSessionDate.js
@@ -5,11 +5,11 @@ function getPrevSessionDate(tp) {
                     .where(page => {
                         if (page.type === "session") return true;
                     })
-                    .sort(page => page.sessionNum, "desc")[1]
+                    .sort(page => page.sessionNum, "desc")[1] // Issue when only session 0
     // [0] is the new session with tags in the date
     let formattedDate;
     if (prevSession.date.c) {
-        formattedDate = `${prevSession.year}-${String(prevSession.month).padStart(2, '0')}-${String(prevSession.day).padStart(2, '0')}`;console.log(prevSession);
+        formattedDate = `${prevSession.date.year}-${String(prevSession.date.month).padStart(2, '0')}-${String(prevSession.date.day).padStart(2, '0')}`;
     } else {
         // So I could do a some future sessions 
         formattedDate = prevSession.date;
